f1b050cd371d9cde68e4443b8134d192c771490b
BrentSnoddy <brentsnoddy1@gmail.com>
2021-08-06 18:06:57 -0400

    Rewind... Reverse a string with ease!

--- js/site.js
+++ js/site.js
@@ -1,5 +1,39 @@
-//Call Hello World
+//Controller Function
+// GET THE STRING FROM THE PAGE 
+function getString(){
+    document.getElementById("alert").classList.add("invisible");
 
-function HelloWorld(){
-    alert("Hello World");
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+//Logic Function
+// REVERSE THE STRING
+function reverseString(userString){
+
+    let revString = [];
+
+
+    //let name = "BOBBY";
+    //name[0] = "B";
+    //name[4] = "y";
+    //last position in an array is name.length - 1
+
+    //reverse a string using a "FOR LOOP"
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    return revString;
+}
+// Display Function
+// DISPLAY REVERSE STRING TO THE USER
+function displayString(revString){
+
+    //write message to page
+    document.getElementById("msg").innerHTML = `Your String Reversed is: ${revString}`
+    //turn on alert box
+    document.getElementById("alert").classList.remove("invisible")
 }
